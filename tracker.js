05c3578865291d36f370db1238530b7244a39d70
cpoclinch <carlos.poclin@clipboardhealth.com>
2022-07-05 22:13:23 -0500

    feat(tracker): finish implementation

---
+++ tracker.js
@@ -0,0 +1,56 @@
+const nextServerNumber = (serverNumbers) => {
+  let lowestServerNumber = 1;
+  let alreadyFound = false;
+
+  while (!alreadyFound) {
+    if (serverNumbers.indexOf(lowestServerNumber) >= 0) {
+      lowestServerNumber += 1;
+    } else {
+      alreadyFound = true;
+    }
+  }
+
+  return lowestServerNumber;
+};
+
+class Tracker {
+  hostNames = {};
+
+  allocate = (hostType) => {
+    if (!this.hostNames[hostType]) {
+      this.hostNames[hostType] = [];
+    }
+
+    const nextNumber = nextServerNumber(this.hostNames[hostType]);
+    this.hostNames[hostType].push(nextNumber);
+
+    return `${hostType}${nextNumber}`;
+  };
+
+  deallocate = (hostName) => {
+    const matches = hostName.match(/([a-z]+)(\d+)/);
+    if (!Array.isArray(matches)) {
+      console.log(`${hostName} is not a valid hostName`);
+      return;
+    }
+
+    const hostType = matches[1];
+    const serverNumber = parseInt(matches[2], 10);
+
+    if (
+      this.hostNames[hostType] &&
+      this.hostNames[hostType].indexOf(serverNumber) !== -1
+    ) {
+      const index = this.hostNames[hostType].indexOf(serverNumber);
+      this.hostNames[hostType].splice(index, 1);
+    }
+  };
+}
+
+const tracker = new Tracker();
+
+console.log(tracker.allocate('apibox'));
+console.log(tracker.allocate('apibox'));
+tracker.deallocate('apibox1');
+console.log(tracker.allocate('apibox'));
+console.log(tracker.allocate('sitebox'));
